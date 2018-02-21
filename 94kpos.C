3ac34955c963ef2a65475a90b4bbf4010c529162
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-21 16:45:52 +0530

    94kpos.C

---
+++ 94kpos.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,a[10],i;
+ printf("\n enter the value and pos ");
+ scanf("%d %d",&a,&b);
+ for(i=0;i<a;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ printf("\n the value in %d pos is:%d',k,a[k-1]);
+ return 0;
+ }
